a4e9ff5f7b3e285f6a6e2c8a06e06abf14c28617
“philippe <philippe@dissitou.org>
2022-10-22 19:01:57 +0200

    Squashed commit of the following:
    commit 539d60f2337ac9a4574b598fa483e0fc50bcccf8
    Author: “philippe <“philippe@dissitou.org”>
    Date:   Sat Oct 22 18:43:24 2022 +0200
    
        Update _admin.php
    
    commit 1980da9370d34ad23ed47c35f8b5c11ca4e3c8f5
    Author: “philippe <“philippe@dissitou.org”>
    Date:   Sat Oct 22 18:08:20 2022 +0200
    
        Update _admin.php

--- _admin.php
+++ _admin.php
@@ -93,7 +93,7 @@ class relatedEntriesPostBehaviors
         '$(document).ready(function() {' . "\n" .
             '$(\'#relatedEntries-area label\').toggleWithLegend($(\'#relatedEntries-list\'), {' . "\n" .
                 'legend_click: true,' . "\n" .
-                'cookie: \'dcx_relatedEntries_detail\'' . "\n" .
+                'user_pref: \'dcx_relatedEntries_detail\'' . "\n" .
 
             '});' . "\n" .
             '$(\'a.link-remove\').click(function() {' . "\n" .
@@ -139,7 +139,7 @@ class relatedEntriesPostBehaviors
         if (!$meta_rs) {
             echo
                 '<div class="area" id="relatedEntries-area">' .
-                '<p class="smart-title">' . __('Related posts') . '</p>' .
+                '<label class="bold" for="relatedEntries-list">'.__('Related posts').'</label>'.
                 '<div id="relatedEntries-list" >' .
                 '<p>' . __('No related posts') . '</p>' .
                 '<p><a href="' . $p_url . '&amp;id=' . $id . '&upd=1">' . __('Add links to related posts') . '</a></p>' .
@@ -148,7 +148,7 @@ class relatedEntriesPostBehaviors
         } else {
             echo
                 '<div class="area" id="relatedEntries-area">' .
-                '<p class="smart-title">' . __('Links to related posts') . '</p>' .
+                '<label class="bold" for="relatedEntries-list">'.__('Links to related posts').'</label>'.
                 '<div id="relatedEntries-list" >';
 
             // Get related posts
