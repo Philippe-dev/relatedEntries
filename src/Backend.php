526042378bfd7a5e373c8d43a4812db459fc5368
“philippe <philippe@dissitou.org>
2023-03-15 17:40:40 +0100

    wip

--- src/Backend.php
+++ src/Backend.php
@@ -14,12 +14,18 @@ declare(strict_types=1);
 
 namespace Dotclear\Plugin\relatedEntries;
 
+use adminUserPref;
+use dcPager;
 use dcAdmin;
 use dcAuth;
 use dcCore;
 use dcFavorites;
 use dcPage;
 use dcNsProcess;
+use dt;
+use form;
+use html;
+use http;
 
 class Backend extends dcNsProcess
 {
@@ -43,7 +49,7 @@ class Backend extends dcNsProcess
             preg_match('/' . preg_quote(dcCore::app()->adminurl->get('admin.plugin.relatedEntries')) . '(&.*)?$/', $_SERVER['REQUEST_URI']),
             dcCore::app()->auth->check(dcCore::app()->auth->makePermissions([dcAuth::PERMISSION_CONTENT_ADMIN]), dcCore::app()->blog->id)
         );
-        
+
         /* Register favorite */
         dcCore::app()->addBehavior('adminDashboardFavoritesV2', function (dcFavorites $favs) {
             $favs->register('relatedEntries', [
@@ -55,6 +61,142 @@ class Backend extends dcNsProcess
             ]);
         });
 
+        dcCore::app()->addBehavior('adminPageHelpBlock', [self::class,  'adminPageHelpBlock']);
+        dcCore::app()->addBehavior('adminPostHeaders', [self::class,  'postHeaders']);
+        dcCore::app()->addBehavior('adminPostForm', [self::class,  'adminPostForm']);
+
+        if (isset($_GET['id']) && isset($_GET['r_id'])) {
+            try {
+                $meta  = dcCore::app()->meta;
+                $id    = $_GET['id'];
+                $r_ids = $_GET['r_id'];
+
+                foreach ($meta->splitMetaValues($r_ids) as $tag) {
+                    $meta->delPostMeta($id, 'relatedEntries', $tag);
+                    $meta->delPostMeta($tag, 'relatedEntries', $id);
+                }
+
+                http::redirect(DC_ADMIN_URL . 'post.php?id=' . $id . '&del=1&upd=1');
+            } catch (Exception $e) {
+                dcCore::app()->error->add($e->getMessage());
+            }
+        }
+
         return true;
+    }
+
+    public static function adminPageHelpBlock($blocks)
+    {
+        $found = false;
+        foreach ($blocks as $block) {
+            if ($block == 'core_post') {
+                $found = true;
+
+                break;
+            }
+        }
+        if (!$found) {
+            return null;
+        }
+        $blocks[] = 'relatedEntries_post';
+    }
+
+    public static function postHeaders()
+    {
+        $s = dcCore::app()->blog->settings->relatedEntries;
+        if (!$s->relatedEntries_enabled) {
+            return;
+        }
+
+        return
+        '<script>' . "\n" .
+        '$(document).ready(function() {' . "\n" .
+            '$(\'#relatedEntries-area label\').toggleWithLegend($(\'#relatedEntries-list\'), {' . "\n" .
+                'legend_click: true,' . "\n" .
+                'user_pref: \'dcx_relatedEntries_detail\'' . "\n" .
+
+            '});' . "\n" .
+            '$(\'a.link-remove\').click(function() {' . "\n" .
+            'msg = \'' . __('Are you sure you want to remove this link to a related post?') . '\';' . "\n" .
+            'if (!window.confirm(msg)) {' . "\n" .
+                'return false;' . "\n" .
+            '}' . "\n" .
+            '});' . "\n" .
+            '$(\'a.links-remove\').click(function() {' . "\n" .
+            'msg = \'' . __('Are you sure you want to remove all links to related posts?') . '\';' . "\n" .
+            'if (!window.confirm(msg)) {' . "\n" .
+                'return false;' . "\n" .
+            '}' . "\n" .
+            '});' . "\n" .
+        '});' . "\n" .
+        '</script>' .
+        '<style type="text/css">' . "\n" .
+        'a.links-remove {' . "\n" .
+        'color : #900;' . "\n" .
+        '}' . "\n" .
+        '</style>';
+    }
+
+    public static function adminPostForm($post)
+    {
+        $s = dcCore::app()->blog->settings->relatedEntries;
+
+        $postTypes = ['post'];
+
+        if (!$s->relatedEntries_enabled) {
+            return;
+        }
+        if (is_null($post) || !in_array($post->post_type, $postTypes)) {
+            return;
+        }
+
+        $id      = $post->post_id;
+        $type    = $post->post_type;
+        $meta    = dcCore::app()->meta;
+        $meta_rs = $meta->getMetaStr($post->post_meta, 'relatedEntries');
+
+        if (!$meta_rs) {
+            echo
+                '<div class="area" id="relatedEntries-area">' .
+                '<label class="bold" for="relatedEntries-list">' . __('Links to related posts:') . '</label>' .
+                '<div id="relatedEntries-list" >' .
+                '<p>' . __('No related posts') . '</p>' .
+                '<p><a href="' . DC_ADMIN_URL . 'plugin.php?p=relatedEntries&amp;id=' . $id . '&upd=1">' . __('Add links to related posts') . '</a></p>' .
+                '</div>' .
+                '</div>';
+        } else {
+            echo
+                '<div class="area" id="relatedEntries-area">' .
+                '<label class="bold" for="relatedEntries-list">' . __('Links to related posts:') . '</label>' .
+                '<div id="relatedEntries-list" >';
+
+            // Get related posts
+            try {
+                $params['post_id']    = $meta->splitMetaValues($meta_rs);
+                $params['no_content'] = true;
+                $params['post_type']  = ['post'];
+                $posts                = dcCore::app()->blog->getPosts($params);
+                $counter              = dcCore::app()->blog->getPosts($params, true);
+                //$post_list            = new adminRelatedPostMiniList(dcCore::app(), $posts, $counter->f(0));
+                dcCore::app()->admin->post_list = new BackendList($posts, $counter->f(0));
+            } catch (Exception $e) {
+                dcCore::app()->error->add($e->getMessage());
+            }
+            dcCore::app()->admin->page        = !empty($_GET['page']) ? max(1, (int) $_GET['page']) : 1;
+            dcCore::app()->admin->nb_per_page = adminUserPref::getUserFilters('pages', 'nb');
+
+            echo
+                '<div id="form-entries">' .
+                dcCore::app()->admin->post_list->display(dcCore::app()->admin->page, dcCore::app()->admin->nb_per_page) .
+                '</div>';
+            echo
+
+            '<p class="two-boxes"><a href="' . DC_ADMIN_URL . 'plugin.php?p=relatedEntries&amp;id=' . $id . '"><strong>' . __('Add links to related posts') . '</strong></a></p>' .
+            '<p class="two-boxes right"><a class="links-remove delete" href="' . DC_ADMIN_URL . 'plugin.php?p=relatedEntries&amp;id=' . $id . '&amp;r_id=' . $meta_rs . '&upd=1">' . __('Remove all links to related posts') . '</a></p>' .
+
+            form::hidden(['relatedEntries'], $meta_rs) .
+            '</div>' .
+            '</div>';
+        }
     }
 }
--- src/Frontend.php
+++ src/Frontend.php
@@ -18,6 +18,7 @@ use dcCore;
 use dcNsProcess;
 use html;
 use path;
+use l10n;
 
 class Frontend extends dcNsProcess
 {
@@ -34,8 +35,12 @@ class Frontend extends dcNsProcess
             return false;
         }
 
-        dcCore::app()->addBehavior('publicHeadContent', [self::class, 'publicHeadContent']);
-        dcCore::app()->addBehavior('publicFooterContent', [self::class, 'publicFooterContent']);
+        // require dirname(__FILE__) . '/_widget.php';
+        dcCore::app()->addBehavior('publicEntryBeforeContent', [self::class,  'publicEntryBeforeContent']);
+        dcCore::app()->addBehavior('publicEntryAfterContent', [self::class,  'publicEntryAfterContent']);
+        dcCore::app()->addBehavior('publicHeadContent', [self::class,  'publicHeadContent']);
+
+        l10n::set(dirname(__FILE__) . '/locales/' . dcCore::app()->lang . '/main');
 
         return true;
     }
@@ -44,157 +49,151 @@ class Frontend extends dcNsProcess
     {
         // Settings
 
-        $s = dcCore::app()->blog->settings->colorbox;
+        $s = dcCore::app()->blog->settings->relatedEntries;
 
-        if (!$s->colorbox_enabled) {
+        if (!$s->relatedEntries_enabled) {
             return;
         }
 
-        $url = dcCore::app()->blog->getQmarkURL() . 'pf=colorbox';
+        $url = dcCore::app()->blog->getQmarkURL() . 'pf=' . basename(dirname(__FILE__));
 
         echo
-        '<link rel="stylesheet" type="text/css" href="' . $url . '/css/colorbox_common.css" />' . "\n" .
-        '<link rel="stylesheet" type="text/css" href="' . $url . '/themes/' . $s->colorbox_theme . '/colorbox_theme.css" />' . "\n";
+        '<link rel="stylesheet" type="text/css" href="' . $url . '/css/style.css" />' . "\n";
+    }
 
-        if ($s->colorbox_user_files) {
-            $public_path        = dcCore::app()->blog->public_path;
-            $public_url         = dcCore::app()->blog->settings->system->public_url;
-            $colorbox_user_path = $public_path . '/colorbox/themes/';
-            $colorbox_user_url  = $public_url . '/colorbox/themes/';
+    public static function thisPostrelatedEntries($id)
+    {
+        $meta                 = dcCore::app()->meta;
+        $params['post_id']    = $id;
+        $params['no_content'] = false;
+        $params['post_type']  = ['post'];
 
-            if (file_exists($colorbox_user_path . $s->colorbox_theme . '/colorbox_user.css')) {
-                echo
-                '<link rel="stylesheet" type="text/css" href="' . $colorbox_user_url . $s->colorbox_theme . '/colorbox_user.css" />' . "\n";
-            }
-        } else {
-            $theme_path         = path::fullFromRoot(dcCore::app()->blog->settings->system->themes_path . '/' . dcCore::app()->blog->settings->system->theme, DC_ROOT);
-            $theme_url          = dcCore::app()->blog->settings->system->themes_url . '/' . dcCore::app()->blog->settings->system->theme;
-            $colorbox_user_path = $theme_path . '/colorbox/themes/' . $s->colorbox_theme . '/colorbox_user.css';
-            $colorbox_user_url  = $theme_url . '/colorbox/themes/' . $s->colorbox_theme . '/colorbox_user.css';
-            if (file_exists($colorbox_user_path)) {
-                echo
-                '<link rel="stylesheet" type="text/css" href="' . $colorbox_user_url . '" />' . "\n";
+        $rs = dcCore::app()->blog->getPosts($params);
+
+        return $meta->getMetaStr($rs->post_meta, 'relatedEntries');
+    }
+
+    public static function publicEntryBeforeContent($core, $_ctx)
+    {
+        // Settings
+
+        $s = dcCore::app()->blog->settings->relatedEntries;
+
+        if (!$s->relatedEntries_enabled) {
+            return;
+        }
+        if (!$s->relatedEntries_beforePost) {
+            return;
+        }
+        if (dcCore::app()->ctx->posts->post_type == 'post' && self::thisPostrelatedEntries(dcCore::app()->ctx->posts->post_id) != '') {
+            //related entries
+            $meta = dcCore::app()->meta;
+
+            $r_ids             = self::thisPostrelatedEntries(dcCore::app()->ctx->posts->post_id);
+            $params['post_id'] = $meta->splitMetaValues($r_ids);
+            $rs                = dcCore::app()->blog->getPosts($params);
+
+            if (dcCore::app()->plugins->moduleExists('listImages') && $s->relatedEntries_images) {
+                //images display options
+                $img_options = unserialize($s->relatedEntries_images_options);
+
+                $size     = $img_options['size'] ? $img_options['size'] : 't';
+                $html_tag = $img_options['html_tag'] ? $img_options['html_tag'] : 'div';
+                $link     = $img_options['link'] ? $img_options['link'] : 'entry';
+                $exif     = $img_options['exif'] ? $img_options['exif'] : 0;
+                $legend   = $img_options['legend'] ? $img_options['legend'] : 'none';
+                $bubble   = $img_options['bubble'] ? $img_options['bubble'] : 'image';
+                $from     = $img_options['from'] ? $img_options['from'] : 'full';
+                $start    = $img_options['start'] ? $img_options['start'] : 1;
+                $length   = $img_options['length'] ? $img_options['length'] : 1;
+                $class    = $img_options['class'] ? $img_options['class'] : '';
+                $alt      = $img_options['alt'] ? $img_options['alt'] : 'inherit';
+                $img_dim  = $img_options['img_dim'] ? $img_options['img_dim'] : 0;
+                $def_size = 'o';
+                $ret      = $s->relatedEntries_title != '' ? '<h3>' . $s->relatedEntries_title . '</h3>' : '';
+                $ret .= '<' . ($html_tag == 'li' ? 'ul' : 'div') . ' class="relatedEntries">';
+
+                //listImages plugin comes here
+                while ($rs->fetch()) {
+                    $ret .= tplEntryImages::EntryImagesHelper($size, $html_tag, $link, $exif, $legend, $bubble, $from, $start, $length, $class, $alt, $img_dim, $def_size, $rs);
+                }
+
+                $ret .= '</' . ($html_tag == 'li' ? 'ul' : 'div') . '>' . "\n";
+
+                echo $ret;
+            } elseif (!dcCore::app()->plugins->moduleExists('listImages') || !$s->relatedEntries_images) {
+                $ret = $s->relatedEntries_title != '' ? '<h3>' . $s->relatedEntries_title . '</h3>' : '';
+                $ret .= '<ul class="relatedEntries">';
+
+                while ($rs->fetch()) {
+                    $ret .= '<li><a href="' . $rs->getURL() . '" title="' . html::escapeHTML($rs->post_title) . '">' . $rs->post_title . '</a></li>';
+                }
+                $ret .= '</ul>';
+
+                echo $ret;
             }
         }
     }
 
-    public static function publicFooterContent($core)
+    public static function publicEntryAfterContent($core, $_ctx)
     {
         // Settings
 
-        $s = dcCore::app()->blog->settings->colorbox;
+        $s = dcCore::app()->blog->settings->relatedEntries;
 
-        if (!$s->colorbox_enabled) {
+        if (!$s->relatedEntries_enabled) {
             return;
         }
-
-        $url = dcCore::app()->blog->getQmarkURL() . 'pf=colorbox';
-
-        $icon_name   = 'zoom.png';
-        $icon_width  = '16';
-        $icon_height = '16';
-
-        echo
-        '<script src="' . $url . '/js/jquery.colorbox-min.js"></script>' . "\n" .
-        '<script>' . "\n" .
-        "//<![CDATA[\n";
-
-        $selectors = '.post' . ($s->colorbox_selectors !== '' ? ',' . $s->colorbox_selectors : '');
-
-        echo
-        '$(function () {' . "\n" .
-            'var count = 0; ' .
-            '$("' . $selectors . '").each(function() {' . "\n" .
-                'count++;' . "\n" .
-                '$(this).find(\'a[href$=".jpg"],a[href$=".jpeg"],a[href$=".png"],a[href$=".gif"],' .
-                'a[href$=".JPG"],a[href$=".JPEG"],a[href$=".PNG"],a[href$=".GIF"]\').addClass("colorbox_zoom");' . "\n" .
-                '$(this).find(\'a[href$=".jpg"],a[href$=".jpeg"],a[href$=".png"],a[href$=".gif"],' .
-                'a[href$=".JPG"],a[href$=".JPEG"],a[href$=".PNG"],a[href$=".GIF"]\').attr("rel", "colorbox-"+count);' . "\n";
-
-        if ($s->colorbox_zoom_icon_permanent) {
-            echo
-            '$(this).find("a.colorbox_zoom").each(function(){' . "\n" .
-                'var p = $(this).find("img");' . "\n" .
-                'if (p.length != 0){' . "\n" .
-                    'var offset = p.offset();' . "\n" .
-                    'var parent = p.offsetParent();' . "\n" .
-                    'var offsetparent = parent.offset();' . "\n" .
-                    'var parenttop = offsetparent.top;' . "\n" .
-                    'var parentleft = offsetparent.left;' . "\n" .
-                    'var top = offset.top-parenttop;' . "\n";
-
-            if ($s->colorbox_position) {
-                echo 'var left = offset.left-parentleft;' . "\n";
-            } else {
-                echo 'var left = offset.left-parentleft+p.outerWidth()-' . $icon_width . ';' . "\n";
-            }
-
-            echo '$(this).append("<span style=\"z-index:10;width:' . $icon_width . 'px;height:' . $icon_height . 'px;top:' . '"+top+"' . 'px;left:' . '"+left+"' . 'px;background: url(' . html::escapeJS($url) . '/themes/' . $s->colorbox_theme . '/images/zoom.png) top left no-repeat; position:absolute;\"></span>");' . "\n" .
-                '}' . "\n" .
-            '});' . "\n";
+        if (!$s->relatedEntries_afterPost) {
+            return;
         }
+        if (dcCore::app()->ctx->posts->post_type == 'post' && self::thisPostrelatedEntries(dcCore::app()->ctx->posts->post_id) != '') {
+            //related entries
+            $meta = dcCore::app()->meta;
 
-        if ($s->colorbox_zoom_icon && !$s->colorbox_zoom_icon_permanent) {
-            echo
-            '$(\'body\').prepend(\'<img id="colorbox_magnify" style="display:block;padding:0;margin:0;z-index:10;width:' . $icon_width . 'px;height:' . $icon_height . 'px;position:absolute;top:0;left:0;display:none;" src="' . html::escapeJS($url) . '/themes/' . $s->colorbox_theme . '/images/zoom.png" alt=""  />\');' . "\n" .
-            '$(\'img#colorbox_magnify\').on(\'click\', function ()' . "\n" .
-                '{ ' . "\n" .
-                    '$("a.colorbox_zoom img.colorbox_hovered").click(); ' . "\n" .
-                    '$("a.colorbox_zoom img.colorbox_hovered").removeClass(\'colorbox_hovered\');' . "\n" .
-                '});' . "\n" .
-                '$(\'a.colorbox_zoom img\').on(\'click\', function ()' . "\n" .
-                '{ ' . "\n" .
-                    '$(this).removeClass(\'colorbox_hovered\');' . "\n" .
-                '});' . "\n" .
-                '$("a.colorbox_zoom img").hover(function(){' . "\n" .
+            $r_ids             = self::thisPostrelatedEntries(dcCore::app()->ctx->posts->post_id);
+            $params['post_id'] = $meta->splitMetaValues($r_ids);
+            $rs                = dcCore::app()->blog->getPosts($params);
 
-                'var p = $(this);' . "\n" .
-                'p.addClass(\'colorbox_hovered\');' . "\n" .
-                'var offset = p.offset();' . "\n";
+            if (dcCore::app()->plugins->moduleExists('listImages') && $s->relatedEntries_images) {
+                //images display options
+                $img_options = unserialize($s->relatedEntries_images_options);
 
-            if (!$s->colorbox_position) {
-                echo '$(\'img#colorbox_magnify\').css({\'top\' : offset.top, \'left\' : offset.left+p.outerWidth()-' . $icon_width . '});' . "\n";
-            } else {
-                echo '$(\'img#colorbox_magnify\').css({\'top\' : offset.top, \'left\' : offset.left});' . "\n";
-            }
-            echo
-            '$(\'img#colorbox_magnify\').show();' . "\n" .
-            '},function(){' . "\n" .
-                'var p = $(this);' . "\n" .
-                'p.removeClass(\'colorbox_hovered\');' . "\n" .
-                '$(\'img#colorbox_magnify\').hide();' . "\n" .
-            '});' . "\n";
-        }
+                $size     = $img_options['size'] ? $img_options['size'] : 't';
+                $html_tag = $img_options['html_tag'] ? $img_options['html_tag'] : 'div';
+                $link     = $img_options['link'] ? $img_options['link'] : 'entry';
+                $exif     = $img_options['exif'] ? $img_options['exif'] : 0;
+                $legend   = $img_options['legend'] ? $img_options['legend'] : 'none';
+                $bubble   = $img_options['bubble'] ? $img_options['bubble'] : 'image';
+                $from     = $img_options['from'] ? $img_options['from'] : 'full';
+                $start    = $img_options['start'] ? $img_options['start'] : 1;
+                $length   = $img_options['length'] ? $img_options['length'] : 1;
+                $class    = $img_options['class'] ? $img_options['class'] : '';
+                $alt      = $img_options['alt'] ? $img_options['alt'] : 'inherit';
+                $img_dim  = $img_options['img_dim'] ? $img_options['img_dim'] : 0;
+                $def_size = 'o';
+                $ret      = $s->relatedEntries_title != '' ? '<h3>' . $s->relatedEntries_title . '</h3>' : '';
+                $ret .= '<' . ($html_tag == 'li' ? 'ul' : 'div') . ' class="relatedEntries">';
 
-        foreach (unserialize($s->colorbox_advanced) as $k => $v) {
-            if ($v === '') {
-                if ($k == 'title' && $s->colorbox_legend == 'alt') {
-                    $opts[] = $k . ': function(){return $(this).find(\'img\').attr(\'alt\');}';
-                } elseif ($k == 'title' && $s->colorbox_legend == 'title') {
-                    $opts[] = $k . ': function(){return $(this).attr(\'title\');}';
-                } elseif ($k == 'title' && $s->colorbox_legend == 'none') {
-                    $opts[] = $k . ': \'\'';
-                } else {
-                    $opts[] = $k . ': false';
+                //listImages plugin comes here
+                while ($rs->fetch()) {
+                    $ret .= tplEntryImages::EntryImagesHelper($size, $html_tag, $link, $exif, $legend, $bubble, $from, $start, $length, $class, $alt, $img_dim, $def_size, $rs);
                 }
-            } elseif (is_bool($v)) {
-                $opts[] = $k . ': ' . ($v ? 'true' : 'false');
-            } elseif (is_numeric($v)) {
-                $opts[] = $k . ': ' . $v;
-            } elseif (is_string($v)) {
-                if ($k == 'onOpen' || $k == 'onLoad' || $k == 'onComplete' || $k == 'onCleanup' || $k == 'onClosed') {
-                    $opts[] = $k . ': function(){return ' . $v . '}';
-                } else {
-                    $opts[] = $k . ": '" . $v . "'";
+
+                $ret .= '</' . ($html_tag == 'li' ? 'ul' : 'div') . '>' . "\n";
+
+                echo $ret;
+            } elseif (!dcCore::app()->plugins->moduleExists('listImages') || !$s->relatedEntries_images) {
+                $ret = $s->relatedEntries_title != '' ? '<h3>' . $s->relatedEntries_title . '</h3>' : '';
+                $ret .= '<ul class="relatedEntries">';
+
+                while ($rs->fetch()) {
+                    $ret .= '<li><a href="' . $rs->getURL() . '" title="' . html::escapeHTML($rs->post_title) . '">' . $rs->post_title . '</a></li>';
                 }
+                $ret .= '</ul>';
+
+                echo $ret;
             }
         }
-
-        echo
-        "});\n" .
-        '$("a[rel*=\'colorbox-\']").colorbox({' . implode(",\n", $opts) . '});' . "\n" .
-        "});\n" .
-        "\n//]]>\n" .
-        "</script>\n";
     }
 }
