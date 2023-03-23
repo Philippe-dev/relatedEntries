0ebe0d21208fa76371bee9a78e6410cda768b157
“philippe <philippe@dissitou.org>
2023-03-23 15:55:06 +0100

    Update Manage.php

--- src/Manage.php
+++ src/Manage.php
@@ -610,7 +610,6 @@ class Manage extends dcNsProcess
                     '<input type="hidden" name="p" value="relatedEntries" />' .
                     form::hidden(['tab'], 'postslist') .
                     form::hidden(['p'], 'relatedEntries') .
-                    
                     dcCore::app()->adminurl->getHiddenFormFields('admin.plugin.relatedEntries', dcCore::app()->admin->post_filter->values()) .
                     dcCore::app()->formNonce() . '</p>' .
                     '</div>' .
