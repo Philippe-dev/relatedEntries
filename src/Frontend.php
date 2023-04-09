f4f6b830de35f2df5baf82b0406255dc8251108c
“philippe <philippe@dissitou.org>
2023-04-09 08:22:44 +0200

    Update Frontend.php

--- src/Frontend.php
+++ src/Frontend.php
@@ -61,7 +61,20 @@ class Frontend extends dcNsProcess
         '<link rel="stylesheet" type="text/css" href="' . $url . '/css/style.css" />' . "\n";
     }
 
-    public static function publicEntryBeforeContent()
+    public static function thisPostrelatedEntries($id)
+    {
+        $meta                 = dcCore::app()->meta;
+        $params['post_id']    = $id;
+        $params['no_content'] = false;
+        $params['post_type']  = ['post'];
+        dcCore::app()->blog->withoutPassword(false);
+        
+        $rs = dcCore::app()->blog->getPosts($params);
+
+        return $meta->getMetaStr($rs->post_meta, 'relatedEntries');
+    }
+
+    public static function publicEntryBeforeContent($core, $_ctx)
     {
         // Settings
 
@@ -70,50 +83,16 @@ class Frontend extends dcNsProcess
         if (!$s->relatedEntries_enabled) {
             return;
         }
-
         if (!$s->relatedEntries_beforePost) {
             return;
         }
-
-        return self::relatedEntriesHtml();
-    }
-
-    public static function publicEntryAfterContent()
-    {
-        // Settings
-
-        $s = dcCore::app()->blog->settings->relatedEntries;
-
-        if (!$s->relatedEntries_enabled) {
-            return;
-        }
-
-        if (!$s->relatedEntries_afterPost) {
-            return;
-        }
-
-        return self::relatedEntriesHtml();
-    }
-
-    /**
-     * Public HTML rendering helper
-     *
-     * @return     string
-     */
-    public static function relatedEntriesHtml()
-    {
-        $s = dcCore::app()->blog->settings->relatedEntries;
-
-        if (dcCore::app()->ctx->posts->post_type == 'post' && dcCore::app()->ctx->posts->post_meta != '') {
+        if (dcCore::app()->ctx->posts->post_type == 'post' && self::thisPostrelatedEntries(dcCore::app()->ctx->posts->post_id) != '') {
             //related entries
-            $meta = dcCore::app()->meta;
-
-            $r_ids                = $meta->getMetaStr(dcCore::app()->ctx->posts->post_meta, 'relatedEntries');
-            $params['post_id']    = $meta->splitMetaValues($r_ids);
-            $params['no_content'] = false;
-            $params['post_type']  = ['post'];
+            $meta              = dcCore::app()->meta;
+            $r_ids             = self::thisPostrelatedEntries(dcCore::app()->ctx->posts->post_id);
+            $params['post_id'] = $meta->splitMetaValues($r_ids);
+            $rs                = dcCore::app()->blog->getPosts($params);
             dcCore::app()->blog->withoutPassword(false);
-            $rs = dcCore::app()->blog->getPosts($params);
 
             if (dcCore::app()->plugins->moduleExists('listImages') && $s->relatedEntries_images) {
                 //images display options
@@ -152,7 +131,67 @@ class Frontend extends dcNsProcess
                 }
                 $ret .= '</ul>';
 
-                unset($rs);
+                echo $ret;
+            }
+        }
+    }
+
+    public static function publicEntryAfterContent($core, $_ctx)
+    {
+        // Settings
+
+        $s = dcCore::app()->blog->settings->relatedEntries;
+
+        if (!$s->relatedEntries_enabled) {
+            return;
+        }
+        if (!$s->relatedEntries_afterPost) {
+            return;
+        }
+        if (dcCore::app()->ctx->posts->post_type == 'post' && self::thisPostrelatedEntries(dcCore::app()->ctx->posts->post_id) != '') {
+            //related entries
+            $meta              = dcCore::app()->meta;
+            $r_ids             = self::thisPostrelatedEntries(dcCore::app()->ctx->posts->post_id);
+            $params['post_id'] = $meta->splitMetaValues($r_ids);
+            $rs                = dcCore::app()->blog->getPosts($params);
+            dcCore::app()->blog->withoutPassword(false);
+
+            if (dcCore::app()->plugins->moduleExists('listImages') && $s->relatedEntries_images) {
+                //images display options
+                $img_options = unserialize($s->relatedEntries_images_options);
+
+                $size     = $img_options['size'] ? $img_options['size'] : 't';
+                $html_tag = $img_options['html_tag'] ? $img_options['html_tag'] : 'div';
+                $link     = $img_options['link'] ? $img_options['link'] : 'entry';
+                $exif     = $img_options['exif'] ? $img_options['exif'] : 0;
+                $legend   = $img_options['legend'] ? $img_options['legend'] : 'none';
+                $bubble   = $img_options['bubble'] ? $img_options['bubble'] : 'image';
+                $from     = $img_options['from'] ? $img_options['from'] : 'full';
+                $start    = $img_options['start'] ? $img_options['start'] : 1;
+                $length   = $img_options['length'] ? $img_options['length'] : 1;
+                $class    = $img_options['class'] ? $img_options['class'] : '';
+                $alt      = $img_options['alt'] ? $img_options['alt'] : 'inherit';
+                $img_dim  = $img_options['img_dim'] ? $img_options['img_dim'] : 0;
+                $def_size = 'o';
+                $ret      = $s->relatedEntries_title != '' ? '<h3>' . $s->relatedEntries_title . '</h3>' : '';
+                $ret .= '<' . ($html_tag == 'li' ? 'ul' : 'div') . ' class="relatedEntries">';
+
+                //listImages plugin comes here
+                while ($rs->fetch()) {
+                    $ret .= tplEntryImages::EntryImagesHelper($size, $html_tag, $link, $exif, $legend, $bubble, $from, $start, $length, $class, $alt, $img_dim, $def_size, $rs);
+                }
+
+                $ret .= '</' . ($html_tag == 'li' ? 'ul' : 'div') . '>' . "\n";
+
+                echo $ret;
+            } elseif (!dcCore::app()->plugins->moduleExists('listImages') || !$s->relatedEntries_images) {
+                $ret = $s->relatedEntries_title != '' ? '<h3>' . $s->relatedEntries_title . '</h3>' : '';
+                $ret .= '<ul class="relatedEntries">';
+
+                while ($rs->fetch()) {
+                    $ret .= '<li><a href="' . $rs->getURL() . '" title="' . html::escapeHTML($rs->post_title) . '">' . $rs->post_title . '</a></li>';
+                }
+                $ret .= '</ul>';
 
                 echo $ret;
             }
